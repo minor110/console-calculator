35ab5a7867b855da9833b97db0649f675db9c916
minor110 <filippov.sergey110@gmail.com>
2024-02-06 19:49:20 +0300

    написали первый вариант программы

--- Console calculator/main.swift
+++ Console calculator/main.swift
@@ -5,9 +5,38 @@
 //  Created by Sergey Filippov on 06.02.2024.
 //
 
+// MARK: - Описание проекта и задачи
+/*
+    Программа представляет собой простейший калькулятьр с вводом и выводом значений с помощью консоли.
+ */
+
 import Foundation
 
-print("Hello, World!")
+// Приветствуем пользователя и предлагаем выбрать необходимую операцию.
+print("Добро пожаловать в программу калькулятор. \nВыберите операцию: +, -, * или /.")
 
-var a = 5
-print(a)
+//Пользователь вводит операцию.
+let personalOperation: String? = readLine()
+
+//Предлагаем ввести первое целое число.
+print("Введите первое целое число:")
+let personalValueA = Int(readLine()!)!
+
+//Предлагаем ввести второе целое число.
+print("Введите второе целое число: ")
+let personalValueB = Int(readLine()!)!
+
+// Выводим выражение, которое получилось.
+print("Идет вычисление примера: \(personalValueA) \(personalOperation!) \(personalValueB)" )
+
+var resultValue: Any?
+
+switch personalOperation {
+    case "+": resultValue = personalValueA + personalValueB
+    case "-": resultValue = personalValueA - personalValueB
+    case "*": resultValue = personalValueA * personalValueB
+    case "/": resultValue = personalValueA / personalValueB
+    default: print("Ничего не получилось")
+}
+// Выводим результат:
+print("Результат: \(resultValue as! Int)")
