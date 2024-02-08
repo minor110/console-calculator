73d31629fa15ffc54c32562647a58e6261fa2083
minor110 <filippov.sergey110@gmail.com>
2024-02-08 13:06:15 +0300

    - Добавили историю примеров, котовые ввел пользователь.

--- Console calculator/main.swift
+++ Console calculator/main.swift
@@ -26,44 +26,63 @@ func showResult(_ result: Int) {
 }
 
 
-func calculate(personalOperation: String, personalValueA: Int, personalValueB: Int) {
+func calculate(personalOperation: String, personalValueA: Int, personalValueB: Int) -> Int? {
     switch personalOperation {
-    case "+": showResult(personalValueA + personalValueB)
-    case "-": showResult(personalValueA - personalValueB)
-    case "*": showResult(personalValueA * personalValueB)
+    case "+": return personalValueA + personalValueB
+    case "-": return personalValueA - personalValueB
+    case "*": return personalValueA * personalValueB
     case "/": if personalValueB != 0 {
-                showResult(personalValueA / personalValueB)
+                return personalValueA / personalValueB
                 } else {
                     print("Делить на ноль нельзя!")
+                    return nil
                 }
     default: print("Вы ввели неверную операцию.")
+            return nil
     }
 }
 
 
 // MARK: - Логика программы
+// В массиве history хранится история посчитанных примеров пользователя.
+var history: [String] = []
 
 // Приветствуем пользователя и предлагаем выбрать необходимую операцию.
 print("Добро пожаловать в программу калькулятор.")
 
 while true {
     //Пользователь вводит операцию. Предлагаем ввести первое и второе целое число.
-    let personalOperation = getDataFromUser(description: "Выберите операцию: +, -, * или /. Для завершения роботы введите q")
+    let personalOperation = getDataFromUser(description: "Выберите операцию: +, -, * или /. \nДля завершения роботы введите q \nДля просмотра истории введите h")
     
+    //Выход из программы
     if personalOperation == "q" {
         exit(0)
+    }else if personalOperation == "h" {
+        for example in history {
+            print(example)
+        }
+        continue
     }
+    //Просмотр истории примеров
+    
     
     let personalValueA = getDataFromUser(description: "Введите первое целое число:")
     let personalValueB = getDataFromUser(description: "Введите второе целое число:")
-
+    
+    //Храним пример пользователя
+    let example: String = personalValueA + " " + personalOperation + " " + personalValueB
+    
     // Выводим выражение, которое получилось.
-    print("Идет вычисление примера: \(personalValueA) \(personalOperation) \(personalValueB)" )
+    print("Идет вычисление примера: \(example)" )
 
     //Пишем логику программы, обработку ошибок пользователя, вывод значения
     if let personalValueA = Int(personalValueA) {
         if let personalValueB = Int(personalValueB) {
-            calculate(personalOperation: personalOperation, personalValueA: personalValueA, personalValueB: personalValueB)
+           let result = calculate(personalOperation: personalOperation, personalValueA: personalValueA, personalValueB: personalValueB)
+            if let result = result {
+                showResult(result)
+                history.append(example + " = " + String(result))
+            }
         } else {
             print("Вы ввели некорректное второе число.")
         }
